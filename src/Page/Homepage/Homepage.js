d30a910a281c38d6022b9cc5d4df5d89711b6524
Ngoxuanquy <ngoxuanquy1812@gmail.com>
2023-09-23 23:15:50 +0700

    quy

--- src/Page/Homepage/Homepage.js
+++ src/Page/Homepage/Homepage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../../componmts/header/Header'
 import Fooder from '../../componmts/fooder/Fooder'
+import { Outlet, Link } from "react-router-dom";
 
 function Homepage() {
     return (
         <div>
             <Header />
             <div>Homepage</div>
+            <Link to="/Sanpham">
+                sản phẩm
+            </Link>
             <Fooder />
         </div>
     )
